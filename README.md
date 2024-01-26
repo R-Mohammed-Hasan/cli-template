07e22b1320fdb7724bb8c684d8cb591a8f5b7590
Mohammed Hasan <rafeek.ahmed1968@gmail.com>
2024-01-26 21:59:13 +0530

    Debugging log level added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# cli-template
- 
+# CLI Template
+
+Just a template for creating a CLI app. "debug" library isn't working so added a custom logger. Need to set env variable name DEBUG for
+this to work.
+
+### Alternatives:
+
+https://www.npmjs.com/package/winston
+https://www.npmjs.com/package/bunyan
--- tool/src/logger.js
+++ tool/src/logger.js
@@ -2,10 +2,11 @@ import chalk from "chalk";
 import debug from "debug";
 
 export default function createLogger(name) {
+  console.log("debug", process.env.DEBUG);
   return {
     log: (...args) => console.log(chalk.gray(...args)),
     warning: (...args) => console.log(chalk.yellow(...args)),
     highlight: (...args) => console.log(chalk.bgCyanBright(...args)),
-    debugger: (...args) => debug(name)(...args),
+    debugger: process.env.DEBUG ? console.log : () => {},
   };
 }
